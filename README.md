8cb54b83b20c309020269a17efa6954b8a9c948d
Edd Turtle <eddturtle@gmail.com>
2016-02-16 20:47:50 +0000

    Make repo into an example, after building the composer package

--- README.md
+++ README.md
@@ -1,88 +1,11 @@
-## Direct Upload to S3 (using AWS Signature v4 & PHP)
+## Direct Upload to S3 Example (using AWS Signature v4 & PHP)
 
-[![GitHub license](https://img.shields.io/github/license/mashape/apistatus.svg?style=flat-square)]()
+This is an example repository, showing how the [direct upload package](https://github.com/eddturtle/direct-upload) can be used.
 
-The source code to an article originally on Designed by a Turtle: Direct Upload to S3 (using AWS Signature v4 & PHP)
+For more information or any questions, go to:
+
+https://github.com/eddturtle/direct-upload
+
+or 
 
 http://www.designedbyaturtle.co.uk/2015/direct-upload-to-s3-using-aws-signature-v4-php/
-
---
-
-### How to use it
-
-Fill in these with your AWS details.
-
-```
-define('AWS_ACCESS_KEY', '');
-define('AWS_SECRET', '');
-```
-
-*Remember not to commit these to a public repository*
-
-Call the function and pass in your bucket and the bucket's region name (more details on finding it here: http://amzn.to/1FtPG6r)
-
-    $s3FormDetails = getS3Details('', '');
-
-Make sure your bucket has the correct CORS Configuration, something like:
-
-```
-<?xml version="1.0" encoding="UTF-8"?>
-<CORSConfiguration xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
-    <CORSRule>
-        <AllowedOrigin>*</AllowedOrigin>
-        <AllowedMethod>GET</AllowedMethod>
-        <AllowedMethod>POST</AllowedMethod>
-        <MaxAgeSeconds>3000</MaxAgeSeconds>
-        <AllowedHeader>Authorization</AllowedHeader>
-    </CORSRule>
-</CORSConfiguration>
-
-```
-
---
-
-### How it Works
-
-##### Step 1 - Create the Scope
-```
-$scope = [
-    S3_KEY,
-    $shortDate,
-    S3_REGION,
-    $service,
-    $requestType
-];
-$credentials = implode('/', $scope);
-```
-##### Step 2 - Create the Policy
-```
-$policy = [
-    'expiration' => gmdate('Y-m-d\TG:i:s\Z', strtotime('+6 hours')),
-    'conditions' => [
-        ['bucket' => $s3Bucket],
-        ['acl' => $acl],
-        ['starts-with', '$key', ''],
-        ['starts-with', '$Content-Type', ''],
-        ['success_action_status' => $successStatus],
-        ['x-amz-credential' => $credentials],
-        ['x-amz-algorithm' => $algorithm],
-        ['x-amz-date' => $date],
-        ['x-amz-expires' => $expires],
-    ]
-];
-$base64Policy = base64_encode(json_encode($policy));
-```  
-##### Step 3 - Sign the Keys with sha256
-```
-$dateKey = hash_hmac('sha256', $shortDate, 'AWS4' . AWS_SECRET, true);
-$dateRegionKey = hash_hmac('sha256', $region, $dateKey, true);
-$dateRegionServiceKey = hash_hmac('sha256', $service, $dateRegionKey, true);
-$signingKey = hash_hmac('sha256', $requestType, $dateRegionServiceKey, true);
-
-$signature = hash_hmac('sha256', $base64Policy, $signingKey);
-```
-
-### Notes
-
-* All dates are in UTC - so the gmdate() function is used in PHP.
-* If you want the upload to be public then change the acl from private to public-read.
--- index.php
+++ index.php
@@ -1,89 +1,13 @@
 <?php
 
-// TODO Enter your AWS credentials
-// Note: these can be set as environment variables (with the same name) or constants.
-define('AWS_ACCESS_KEY', '');
-define('AWS_SECRET', '');
+require_once "vendor/autoload.php";
 
-// TODO Enter your bucket and region details (see details below)
-$s3FormDetails = getS3Details('', '');
-
-/**
- * Get all the necessary details to directly upload a private file to S3
- * asynchronously with JavaScript using the Signature V4.
- *
- * @param string $s3Bucket your bucket's name on s3.
- * @param string $region   the bucket's location/region, see here for details: http://amzn.to/1FtPG6r
- * @param string $acl      the visibility/permissions of your file, see details: http://amzn.to/18s9Gv7
- *
- * @return array ['url', 'inputs'] the forms url to s3 and any inputs the form will need.
- */
-function getS3Details($s3Bucket, $region, $acl = 'private') {
-
-    // Options and Settings
-    $awsKey = (!empty(getenv('AWS_ACCESS_KEY')) ? getenv('AWS_ACCESS_KEY') : AWS_ACCESS_KEY);
-    $awsSecret = (!empty(getenv('AWS_SECRET')) ? getenv('AWS_SECRET') : AWS_SECRET);
-
-    $algorithm = "AWS4-HMAC-SHA256";
-    $service = "s3";
-    $date = gmdate("Ymd\THis\Z");
-    $shortDate = gmdate("Ymd");
-    $requestType = "aws4_request";
-    $expires = "86400"; // 24 Hours
-    $successStatus = "201";
-    $url = "//{$s3Bucket}.{$service}-{$region}.amazonaws.com";
-
-    // Step 1: Generate the Scope
-    $scope = [
-        $awsKey,
-        $shortDate,
-        $region,
-        $service,
-        $requestType
-    ];
-    $credentials = implode('/', $scope);
-
-    // Step 2: Making a Base64 Policy
-    $policy = [
-        'expiration' => gmdate('Y-m-d\TG:i:s\Z', strtotime('+6 hours')),
-        'conditions' => [
-            ['bucket' => $s3Bucket],
-            ['acl' => $acl],
-            ['starts-with', '$key', ''],
-            ['starts-with', '$Content-Type', ''],
-            ['success_action_status' => $successStatus],
-            ['x-amz-credential' => $credentials],
-            ['x-amz-algorithm' => $algorithm],
-            ['x-amz-date' => $date],
-            ['x-amz-expires' => $expires],
-        ]
-    ];
-    $base64Policy = base64_encode(json_encode($policy));
-
-    // Step 3: Signing your Request (Making a Signature)
-    $dateKey = hash_hmac('sha256', $shortDate, 'AWS4' . $awsSecret, true);
-    $dateRegionKey = hash_hmac('sha256', $region, $dateKey, true);
-    $dateRegionServiceKey = hash_hmac('sha256', $service, $dateRegionKey, true);
-    $signingKey = hash_hmac('sha256', $requestType, $dateRegionServiceKey, true);
-
-    $signature = hash_hmac('sha256', $base64Policy, $signingKey);
-
-    // Step 4: Build form inputs
-    // This is the data that will get sent with the form to S3
-    $inputs = [
-        'Content-Type' => '',
-        'acl' => $acl,
-        'success_action_status' => $successStatus,
-        'policy' => $base64Policy,
-        'X-amz-credential' => $credentials,
-        'X-amz-algorithm' => $algorithm,
-        'X-amz-date' => $date,
-        'X-amz-expires' => $expires,
-        'X-amz-signature' => $signature
-    ];
-
-    return compact('url', 'inputs');
-}
+$uploader = new \EddTurtle\DirectUpload\Signature(
+    "YOUR_S3_KEY",
+    "YOUR_S3_SECRET",
+    "YOUR_S3_BUCKET",
+    "eu-west-1"
+);
 
 ?>
 
@@ -101,14 +25,12 @@ function getS3Details($s3Bucket, $region, $acl = 'private') {
             <h1>Direct Upload</h1>
 
             <!-- Direct Upload to S3 Form -->
-            <form action="<?php echo $s3FormDetails['url']; ?>"
+            <form action="<?php echo $uploader->getFormUrl(); ?>"
                   method="POST"
                   enctype="multipart/form-data"
                   class="direct-upload">
 
-                <?php foreach ($s3FormDetails['inputs'] as $name => $value) { ?>
-                    <input type="hidden" name="<?php echo $name; ?>" value="<?php echo $value; ?>">
-                <?php } ?>
+                <?php echo $uploader->getFormInputsAsHtml(); ?>
 
                 <!-- Key is the file's name on S3 and will be filled in with JS -->
                 <input type="hidden" name="key" value="">
